6755ff1bae72c3b2e18c2c58e9166fcf747c0bbb
P0p4r7 <popadic79@gmail.com>
2018-10-10 17:06:29 +0200

    initial commit

---
+++ vue-todo-app/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app" class="container">
+    <img class="logo" src="./assets/logo.png">
+    <TodoList/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld"
+import TodoList from "./components/TodoList"
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 24px;
+}
+.logo {
+  display: block;
+  max-width: 150px;
+  margin: 0 auto;
+}
+
+</style>
